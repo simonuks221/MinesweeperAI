02c4256faa4f5d977d8b5a88301093dcaeafdf4b
simonuks221 <simonasriauka@gmail.com>
2022-12-07 16:35:18 +0200

    Rewritten

--- ModelTester.py
+++ ModelTester.py
@@ -1,134 +1,39 @@
 import matplotlib.pyplot as plt
 from tensorflow import keras
-# nuosekliai jungtam neuroniniam tinklui
-from keras import Sequential, Input, Model
-# sluoksniai kuriuos desim i neuronini tinkla
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from keras.layers import Dense, Concatenate, Embedding, Flatten, LSTM
-from keras.models import save_model, load_model
+from keras.models import load_model
 import pandas as pd
 import numpy as np
 import ast
-import random
+from minesweeperGameLogic import GameInstance
+
 D_SIZE = 2
-BOARD_SIZE = 30
-num_mines = 20
+BOARD_SIZE = 15
 
 df = pd.read_csv('{id}x{id}TrainData.csv'.format(id=D_SIZE*2+1))
 
 model = load_model('modelis{id}x{id}.h5'.format(id=D_SIZE*2+1))
 
-# initialize the revealed tiles list with all False values
-revealed = [[False for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
 
-# initialize the board with all zeroes
-board = [[0 for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
-checked = [[0 for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
-
-# define the number of mines on the board
-
-
-def placeMines(board):
-    # place the mines randomly on the board
-    num_mines = 25
-    while num_mines > 0:
-        i = random.randint(0, BOARD_SIZE-1)
-        j = random.randint(0, BOARD_SIZE-1)
-        if board[i][j] != -1:
-            board[i][j] = -1
-            num_mines -= 1
-
-# function to count the number of mines in the 8 surrounding cells
-
-
-def count_adjacent_mines(board, i, j):
-    count = 0
-    for x in range(i-1, i+2):
-        for y in range(j-1, j+2):
-            if x >= 0 and x < BOARD_SIZE and y >= 0 and y < BOARD_SIZE and board[x][y] == -1:
-                count += 1
-    return count
-
-
-# function to reveal a tile
-
-
-def reveal_tile(board, revealed, i, j):
-    if i >= 0 and i < BOARD_SIZE and j >= 0 and j < BOARD_SIZE and not revealed[i][j]:
-        revealed[i][j] = True
-        if board[i][j] == 0:
-            # if the tile is a blank space, also reveal all adjacent tiles
-            reveal_tile(board, revealed, i-1, j)
-            reveal_tile(board, revealed, i+1, j)
-            reveal_tile(board, revealed, i, j-1)
-            reveal_tile(board, revealed, i, j+1)
-            reveal_tile(board, revealed, i-1, j-1)
-            reveal_tile(board, revealed, i-1, j+1)
-            reveal_tile(board, revealed, i+1, j+1)
-            reveal_tile(board, revealed, i+1, j-1)
-
-
-def printOutTHeBoard(board, revealed):
-    for i in range(BOARD_SIZE):
-        for j in range(BOARD_SIZE):
-            if revealed[i][j]:
-                if board[i][j] == -1:
-                    print("*", end=" ")
-                else:
-                    print(board[i][j], end=" ")
-            else:
-                print("#", end=" ")
-        print()
-
-
-def GenerateBoard():
-    global board
-    global revealed
-    global checked
-    revealed = [[False for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
-    # initialize the board with all zeroes
-    board = [[0 for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
-    checked = [[0 for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
-
-    placeMines(board)
-    # go through the board and count the number of mines adjacent to each non-mine cell
-    for i in range(BOARD_SIZE):
-        for j in range(BOARD_SIZE):
-            if board[i][j] != -1:
-                board[i][j] = count_adjacent_mines(board, i, j)
-
-    # Start with empty spaces revealed
-    for i in range(BOARD_SIZE):
-        for j in range(BOARD_SIZE):
-            if board[i][j] == 0:
-                reveal_tile(board, revealed, i, j)
-
-
-def getNeighbours(i, j):
-    global board
-    global revealed
+def getNeighbours(i, j, gm):
     neighbours = []
     for x in range(-D_SIZE, D_SIZE+1):
         for y in range(-D_SIZE, D_SIZE+1):
             if i+x >= 0 and j+y >= 0 and i+x < BOARD_SIZE and j+y < BOARD_SIZE:
-                if not revealed[i+x][j+y]:
+                if not gm.revealed[i+x][j+y]:
                     neighbours.append(9/10)
                 else:
-                    neighbours.append(board[i+x][j+y]/10)
+                    neighbours.append(gm.board[i+x][j+y]/10)
     return neighbours
 
 
-def FindLowestValue():
-    global board
-    global revealed
-
+def FindLowestValue(gm):
     testableTiles = []
     testableTileCoords = []
 
     for i in range(1, BOARD_SIZE):
         for j in range(1, BOARD_SIZE):
-            if not revealed[i][j]:
-                neighbours = getNeighbours(i, j)
+            if not gm.revealed[i][j]:
+                neighbours = getNeighbours(i, j, gm)
                 if len(neighbours) == (1+2*D_SIZE)*(1+2*D_SIZE):
                     testableTiles.append(neighbours)
                     testableTileCoords.append((i, j))
@@ -140,7 +45,8 @@ def FindLowestValue():
     return testableTileCoords[minIndex]
 
 
-GenerateBoard()
+gm = GameInstance(BOARD_SIZE, 10)
+gm.GenerateBoard()
 #printOutTHeBoard(board, revealed)
 
 # main game loop
@@ -148,21 +54,21 @@ roundIndex = 0
 while True:
     roundIndex += 1
     # print the board
-    printOutTHeBoard(board, revealed)
+    gm.printOutTHeBoard()
     # get input from the user
-    (row, col) = FindLowestValue()
+    (row, col) = FindLowestValue(gm)
     print("Round ", roundIndex, " Chosen: ", col, row)
     #row = int(input("Enter row: "))
     #col = int(input("Enter column: "))
 
     # reveal the tile at the specified coordinates
-    reveal_tile(board, revealed, row, col)
+    gm.reveal_tile(row, col)
 
     # check if the game is over
     game_over = False
     for i in range(BOARD_SIZE):
         for j in range(BOARD_SIZE):
-            if board[i][j] == -1 and revealed[i][j]:
+            if gm.board[i][j] == -1 and gm.revealed[i][j]:
                 # if a mine is revealed, the game is over
                 game_over = True
                 break
---
+++ minesweeperGameLogic.py
@@ -0,0 +1,75 @@
+import random
+
+num_mines = 10
+
+
+class GameInstance:
+    def __init__(self, board_size, num_mines):
+        self.BOARD_SIZE = board_size
+        self.num_mines = num_mines
+
+    def placeMines(self):
+        # place the mines randomly on the board
+        num_mines = 25
+        while num_mines > 0:
+            i = random.randint(0, self.BOARD_SIZE-1)
+            j = random.randint(0, self.BOARD_SIZE-1)
+            if self.board[i][j] != -1:
+                self.board[i][j] = -1
+                num_mines -= 1
+
+    def count_adjacent_mines(self, i, j):
+        count = 0
+        for x in range(i-1, i+2):
+            for y in range(j-1, j+2):
+                if x >= 0 and x < self.BOARD_SIZE and y >= 0 and y < self.BOARD_SIZE and self.board[x][y] == -1:
+                    count += 1
+        return count
+
+    def reveal_tile(self, i, j):
+        if i >= 0 and i < self.BOARD_SIZE and j >= 0 and j < self.BOARD_SIZE and not self.revealed[i][j]:
+            self.revealed[i][j] = True
+            if self.board[i][j] == 0:
+                # if the tile is a blank space, also reveal all adjacent tiles
+                self.reveal_tile(i-1, j)
+                self.reveal_tile(i+1, j)
+                self.reveal_tile(i, j-1)
+                self.reveal_tile(i, j+1)
+                self.reveal_tile(i-1, j-1)
+                self.reveal_tile(i-1, j+1)
+                self.reveal_tile(i+1, j+1)
+                self.reveal_tile(i+1, j-1)
+
+    def printOutTHeBoard(self):
+        for i in range(self.BOARD_SIZE):
+            for j in range(self.BOARD_SIZE):
+                if self.revealed[i][j]:
+                    if self.board[i][j] == -1:
+                        print("*", end=" ")
+                    else:
+                        print(self.board[i][j], end=" ")
+                else:
+                    print("#", end=" ")
+            print()
+
+    def GenerateBoard(self):
+        self.revealed = [[False for i in range(self.BOARD_SIZE)]
+                         for j in range(self.BOARD_SIZE)]
+        # initialize the board with all zeroes
+        self.board = [[0 for i in range(self.BOARD_SIZE)]
+                      for j in range(self.BOARD_SIZE)]
+        self.checked = [[0 for i in range(self.BOARD_SIZE)]
+                        for j in range(self.BOARD_SIZE)]
+
+        self.placeMines()
+        # go through the board and count the number of mines adjacent to each non-mine cell
+        for i in range(self.BOARD_SIZE):
+            for j in range(self.BOARD_SIZE):
+                if self.board[i][j] != -1:
+                    self.board[i][j] = self.count_adjacent_mines(i, j)
+
+        # Start with empty spaces revealed
+        for i in range(self.BOARD_SIZE):
+            for j in range(self.BOARD_SIZE):
+                if self.board[i][j] == 0:
+                    self.reveal_tile(i, j)
